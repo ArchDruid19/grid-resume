40302e21b98c827d48298e65dd413ff46c4ee4fd
ArchDruid19 <druidvester@gmail.com>
2025-10-13 11:13:38 -0400

    Testing some javascript responsive stuff

--- scripts/mobile_menu.js
+++ scripts/mobile_menu.js
@@ -1,11 +1,17 @@
 function showMenu() {
     var x = document.getElementById("top-navbar");
-    if (x.style.display === "none") {
-        x.style.display = "inline";
-        var y = document.getElementById("top-flex");
-        y.style.flexDirection = "column";
+    if (window.innerWidth < 800) {
+        if (x.style.display === "none") {
+            x.style.display = "inline";
+            var y = document.getElementById("top-flex");
+            y.style.flexDirection = "column";
+        } else {
+            x.style.display = "none";
+        }
+
     } else {
-        x.style.display = "none";
+        x.style.display = "flex";
     }
+
 }
 
