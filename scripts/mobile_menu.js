6259e671702f79b0ff60ecfcd56f0f61c043aa57
unknown <druidvester@gmail.com>
2025-10-14 16:02:35 -0400

    I dont want to work on this horrible assingment anymore

--- scripts/mobile_menu.js
+++ scripts/mobile_menu.js
@@ -1,17 +1,12 @@
 function showMenu() {
-    var x = document.getElementById("top-navbar");
-    if (window.innerWidth < 800) {
-        if (x.style.display === "none") {
-            x.style.display = "inline";
-            var y = document.getElementById("top-flex");
-            y.style.flexDirection = "column";
-        } else {
-            x.style.display = "none";
-        }
-
+    var top_navbar = document.getElementById("top-navbar");
+    if (top_navbar.style.display === "none") {
+        top_navbar.style.display = "inline";
+        var top_flex_container = document.getElementById("top-flex");
+        top_flex_container.style.flexDirection = "column";
     } else {
-        x.style.display = "flex";
+        top_navbar.style.display = "none";
     }
-
 }
+
 
